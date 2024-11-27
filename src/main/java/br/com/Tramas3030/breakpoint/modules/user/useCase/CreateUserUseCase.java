b0395bf523bf400e2b13d03dd998f3bdc16015cc
Matheus Raphael <matheusraphael0902@gmail.com>
2024-11-27 09:59:38 -0300

    feat(security): Add encrypted passwords

--- src/main/java/br/com/Tramas3030/breakpoint/modules/user/useCase/CreateUserUseCase.java
+++ src/main/java/br/com/Tramas3030/breakpoint/modules/user/useCase/CreateUserUseCase.java
@@ -4,6 +4,7 @@ import br.com.Tramas3030.breakpoint.exceptions.UserFoundException;
 import br.com.Tramas3030.breakpoint.modules.user.entities.UserEntity;
 import br.com.Tramas3030.breakpoint.modules.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +13,16 @@ public class CreateUserUseCase {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public UserEntity execute(UserEntity userEntity) {
     this.userRepository.findByEmail(userEntity.getEmail()).ifPresent(user -> {
       throw new UserFoundException();
     });
+
+    var encryptedPassword = passwordEncoder.encode(userEntity.getPassword());
+    userEntity.setPassword(encryptedPassword);
 
     return this.userRepository.save(userEntity);
   }
