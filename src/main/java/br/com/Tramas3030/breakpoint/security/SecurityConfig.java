c90ed51bff49f1551a507d612ee745bc4ad8a9c7
Matheus Raphael <matheusraphael0902@gmail.com>
2024-11-27 09:38:50 -0300

    feat(security): add Spring Security and configure public routes

---
+++ src/main/java/br/com/Tramas3030/breakpoint/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.Tramas3030.breakpoint.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/user/").permitAll();
+          auth.anyRequest().authenticated();
+        });
+
+    return http.build();
+  }
+
+}
