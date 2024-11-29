f15e1f2705f637849c7cd671d636db22ba6b55f9
Matheus Raphael <matheusraphael0902@gmail.com>
2024-11-29 19:29:44 -0300

    feat(middleware): add filtering middleware

--- src/main/java/br/com/Tramas3030/breakpoint/modules/vice/controllers/ViceController.java
+++ src/main/java/br/com/Tramas3030/breakpoint/modules/vice/controllers/ViceController.java
@@ -3,6 +3,7 @@ package br.com.Tramas3030.breakpoint.modules.vice.controllers;
 import br.com.Tramas3030.breakpoint.modules.vice.dto.ViceListResponseDTO;
 import br.com.Tramas3030.breakpoint.modules.vice.entities.ViceEntity;
 import br.com.Tramas3030.breakpoint.modules.vice.useCase.CreateViceUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user/vice")
@@ -19,8 +22,11 @@ public class ViceController {
   private CreateViceUseCase createViceUseCase;
 
   @PostMapping("/")
-  public ResponseEntity<Object> create(@Valid @RequestBody ViceEntity viceEntity) {
+  public ResponseEntity<Object> create(@Valid @RequestBody ViceEntity viceEntity, HttpServletRequest request) {
     try {
+      var userId = request.getAttribute("user_id");
+      viceEntity.setUserId(UUID.fromString(userId.toString()));
+
       ViceListResponseDTO result = this.createViceUseCase.execute(viceEntity);
       return ResponseEntity.ok().body(result);
     } catch (Exception e) {
