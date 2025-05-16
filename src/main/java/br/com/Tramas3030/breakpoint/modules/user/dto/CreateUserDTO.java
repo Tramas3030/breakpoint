05e404a9f12d6ee41a4bb31fa1620447f548f87e
Matheus Raphael <matheusraphael0902@gmail.com>
2025-05-16 15:29:02 -0300

    refactor: update create user docs

--- src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/CreateUserDTO.java
+++ src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/CreateUserDTO.java
@@ -1,19 +1,23 @@
 package br.com.Tramas3030.breakpoint.modules.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateUserDTO(
     @NotBlank(message = "O campo [nome] não pode ficar em branco")
+    @Schema(example = "Simone Simons")
     String name,
 
     @NotBlank(message = "O campo [email] não pode ficar em branco")
     @Email(message = "O campo [email] deve conter um email válido")
+    @Schema(example = "simonesimons@gmail.com")
     String email,
 
     @NotBlank(message = "O campo [senha] não pode ficar em branco")
     @Length(min = 5, max = 100, message = "A senha deve conter entre (5) e (100) caracteres")
+    @Schema(example = "12345")
     String password
 ) {
 }
--- src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/ResponseCreateUserDTO.java
+++ src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/ResponseCreateUserDTO.java
@@ -16,6 +16,7 @@ public record ResponseCreateUserDTO(
       String email,
 
       @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+      @Schema(type = "string", example = "12/05/2025 15:00")
       LocalDateTime createdAt
 ) {
 }
