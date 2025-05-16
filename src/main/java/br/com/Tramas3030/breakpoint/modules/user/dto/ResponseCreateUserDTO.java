194ca3d133ce2c0199c39cc4979ec8e4da1c545a
Matheus Raphael <matheusraphael0902@gmail.com>
2025-05-16 15:05:08 -0300

    refactor: update ResponseCreateUserDTO

--- src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/ResponseCreateUserDTO.java
+++ src/main/java/br/com/Tramas3030/breakpoint/modules/user/dto/ResponseCreateUserDTO.java
@@ -1,25 +1,21 @@
 package br.com.Tramas3030.breakpoint.modules.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class ResponseCreateUserDTO {
+public record ResponseCreateUserDTO(
+      UUID id,
 
-  private UUID id;
-  @Schema(example = "Simone Simons")
-  private String name;
-  @Schema(example = "simonesimons@gmail.com")
-  private String email;
-  private LocalDateTime createdAt;
+      @Schema(example = "Simone Simons")
+      String name,
 
+      @Schema(example = "simonesimons@gmail.com")
+      String email,
+
+      @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+      LocalDateTime createdAt
+) {
 }
